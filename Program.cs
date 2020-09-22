f6ee458e79c74cc8e0fd60a31e769ca8ebca93a3
oleksenk <71723640+oleksenk@users.noreply.github.com>
2020-09-22 21:08:37 +0300

    Add files via upload
    lab1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Час вільного падіння обчислюється за формулою (t=sqrt((2h)/g))*/
+            double t, h, g = 9.8;
+            Console.WriteLine("Введіть висоту");
+            h = double.Parse(Console.ReadLine());
+            t = (Math.Sqrt((2 * h) / g));
+            Console.WriteLine("Час падіння =" + t);
+        }
+    }
+}
