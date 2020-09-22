c3ba39c1c4a3d25b32db9a4768916efc0a7264e9
oleksenk <71723640+oleksenk@users.noreply.github.com>
2020-09-22 21:10:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float b;
+            float c;
+            float d;
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = float.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            d = b / a;
+            Console.WriteLine(d);
+            if (b * d == c)
+            {
+                Console.WriteLine("Ці числа геометрична прогресія");
+            }
+            if (b * d != c)
+            {
+                Console.WriteLine("Ці числа не геометрична прогресія");
+                }
+        }
+    }
+}
