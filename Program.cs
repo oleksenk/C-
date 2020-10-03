4ca4386481e5b9628e8f1fa92ba3ffbf2914083d
oleksenk <71723640+oleksenk@users.noreply.github.com>
2020-10-03 19:24:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nn, nk, result = 1;
+            int count = 0;
+            Console.WriteLine("Enter nn: ");
+            nn = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter nk: ");
+            nk = float.Parse(Console.ReadLine());
+
+            for (int i = 1; i < nk; i++)
+            {
+
+                if (0 <= nn && nn <= nk)
+                {
+                    count++;
+                    result *= (Math.Pow(-1, (Math.Pow(nn, 2) + nn + 1)) * nn) / (2 * Math.Pow(nn, 2) + 5);
+                }
+            }
+
+            Console.WriteLine("Amount of itterations: " + count + "P=" + result);
+            Console.ReadLine();
+        }
+    }
+}
