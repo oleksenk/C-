53464b29885bf63fc9ca39abc0da0bdcb88ef549
oleksenk <71723640+oleksenk@users.noreply.github.com>
2020-12-21 11:23:59 +0200

    Add files via upload

---
+++ lab11/lab11/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_11.Entities
+{
+    public class Product
+    {
+        public bool SizesTaken { get; set; }
+        public bool CutOff { get; set; }
+        public bool Sharpened { get; set; }
+        public bool Thread { get; set; }
+        public bool Drilled { get; set; }
+        public bool Painted { get; set; }
+        public bool Tested { get; set; }
+        public bool Packed { get; set; }
+
+
+    }
+}
